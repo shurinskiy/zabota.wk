b5981d85af8d7bc9ca4f267bf6de2b2d9e14bb66
_tango <shurinskiy@mail.ru>
2022-11-21 11:13:10 +0600

    add team section

--- src/blocks/team/team.js
+++ src/blocks/team/team.js
@@ -1,10 +1,19 @@
 (() => {
+	const items = document.querySelectorAll('.team__item');
 
-	document.querySelectorAll('.team__item-toggle').forEach((toggle) => {
-		toggle.addEventListener('click', () => {
-			toggle.classList.toggle('hidden');
-			toggle.nextElementSibling.classList.toggle('opened');
+	if (items) {
+		items.forEach(item => {
+			item.querySelector('.team__item-toggle').addEventListener('click', function(e) {
+				items.forEach(item => item.classList.remove('opened'));
+				this.closest('.team__item').classList.toggle('opened');
+			});
+		})
+
+		document.addEventListener('click', (e) => {
+			if([...items].some(item => item.classList.contains('opened')) && !e.target.closest('.team__item')) {
+				items.forEach(item => item.classList.remove('opened'));
+			}
 		});
-	});
+	}
 
 })();
