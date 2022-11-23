1f338ea241042c95a8d44aa64cdb845da50f0dd5
_tango <shurinskiy@mail.ru>
2022-11-23 20:48:11 +0600

    rate table adaptive

--- src/blocks/rate/rate.js
+++ src/blocks/rate/rate.js
@@ -1,4 +1,15 @@
 (() => {
 
+	$('.rate__toggle').on('click', function() {
+		$(this).nextAll('tr').toggleClass('showed');
+	});
+
+	$('.rate__cost').find('button').on('click', function() {
+		$('.rate__table th')
+			.add('.rate__table td')
+			.removeClass('active')
+			.filter(`.${this.id}`)
+			.addClass('active');
+	});
 
 })();
