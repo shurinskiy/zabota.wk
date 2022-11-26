02e3a624a6cbe8d95caf93955e3254abe1c61d86
_tango <shurinskiy@mail.ru>
2022-11-27 05:14:53 +0600

    pace only homepage

--- src/blocks/pace/pace.js
+++ src/blocks/pace/pace.js
@@ -1,10 +1,22 @@
 (() => {
 
-	// console.log(Pace);
-
-	/* Pace.options = {
-		elements: false,
-		restartOnRequestAfter: false,
-	} */
+	if (typeof Pace !== "undefined") {
+		Pace.options = {
+			// minTime: 250,
+			// ghostTime: 100,
+			elements: false,
+			ajax: false,
+			restartOnPushState: false,
+			eventLag: false
+		}
+	
+		Pace.on('done', () => {
+			$('.pace').delay(500).fadeOut(500);
+		});
+	
+		/* Pace.on('hide', () => {
+			console.log('hide');
+		}); */
+	}
 
 })();
