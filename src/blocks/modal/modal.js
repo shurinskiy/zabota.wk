ceae49232e36bd2b0b63c326d03544586fcb36d5
_tango <shurinskiy@mail.ru>
2022-11-26 15:33:00 +0600

    some cases fix

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -8,6 +8,7 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 	makeModalFrame({ el: '.resume__field, .footer__call, .footer__ask-button, .header__button', scrollLock }, function(el) {
 		const agree = this.querySelector('.resume__agree [type="checkbox"]');
 		const submit = this.querySelector('[type="submit"]');
+		const alert = this.querySelector('.resume__alert');
 
 		Inputmask({ 
 			"mask": "+7 (999) 999-99-99", 
@@ -19,47 +20,51 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 			area.addEventListener('input', () => area.style.height = Math.max(area.scrollHeight, area.offsetHeight) + 'px');
 		});
 		
-		if(submit && agree) {
-			let formdata = $(this).find('form').serialize();
+		if (submit && agree && alert) {
+			let id = document.location.pathname.split('/').at(-1).split('.')[0];
+			let formdata = $(this).find('form').serialize() + `&id=${id}`;
+			let required = $(this).find('[required]');
+
 			
 			submit.addEventListener('click', (e) => {
 				e.preventDefault();
 				
-				
-				/* $.ajax({
-					type: 'post',
-					dataType: 'json',
-					url: simplemail.url,
-					data: formdata,
-					cache: false,
-				}).done(function(response) {
+				if ([...required].every(field => field.value)) {
 					
-					if(response.success) {
-						$forminputs.val('');
-						$infoblock.addClass('form__alert_success').html(response.data.text);
+					/* $.ajax({
+						type: 'post',
+						dataType: 'json',
+						url: 'send.php',
+						data: formdata,
+						cache: false,
+					}).done(function(response) {
 						
-						setTimeout(() => {
-							$form
-							.parents('.modal__body')
-							.find('.modal__close')
-							.trigger('click');
-						}, 3000);
+						if(response.success) {
+							$forminputs.val('');
+							$infoblock.addClass('form__alert_success').html(response.data.text);
+							
+							setTimeout(() => {
+								$form
+								.parents('.modal__body')
+								.find('.modal__close')
+								.trigger('click');
+							}, 3000);
+							
+						} else {
+							$infoblock.html(response.data.text);
+		
+							if(response.data.smtpinfo !== undefined && response.data.smtpinfo !== '')
+							console.log(response.data.smtpinfo);
+						}
 						
-					} else {
-						$infoblock.html(response.data.text);
-	
-						if(response.data.smtpinfo !== undefined && response.data.smtpinfo !== '')
-						console.log(response.data.smtpinfo);
-					}
-					
-					$elements.prop("disabled", false);
-				}); */
+						$elements.prop("disabled", false);
+					}); */
+				}
 			});
 
 
 			agree.addEventListener('change', () => submit.toggleAttribute("disabled"));
 		}
 	});
-
 
 })();
