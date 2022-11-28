46614d0f6053e71f49115287c745613df4ecacfe
_tango <shurinskiy@mail.ru>
2022-11-28 18:37:31 +0600

    fix preloader height on phones

--- src/blocks/pace/pace.js
+++ src/blocks/pace/pace.js
@@ -1,6 +1,15 @@
 (() => {
 
 	if (typeof Pace !== "undefined") {
+		const vh = window.innerHeight * 0.01;
+
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+		document.addEventListener('resize', () => {
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		});
+	
 		Pace.options = {
 			// minTime: 250,
 			// ghostTime: 100,
