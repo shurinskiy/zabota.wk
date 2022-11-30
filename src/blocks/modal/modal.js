f36c65625b94c7ce1849b3f667e772e5bbb42343
_tango <shurinskiy@mail.ru>
2022-11-30 23:50:53 +0600

    fix phone fields behaviors

--- src/blocks/modal/modal.js
+++ src/blocks/modal/modal.js
@@ -3,7 +3,20 @@ import scrollLock from 'scroll-lock';
 import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 
 (() => {
-		
+
+	/* Проверка полей на пустое значение. В случае type="tel", нужно хотя бы одно заполненное поле */
+	const fieldsValidation = ([...fields]) => {
+		return fields.reduce((flag, field) => {
+			if (field.type == 'text' || field.tagName == 'TEXTAREA') {
+				flag = !!field.value;
+			} else if(field.type == 'tel') {
+				flag = fields.some(field => (field.type == 'tel') && !!field.value);
+			}
+			
+			return flag;
+		}, false);
+	}
+
 	addUnderlay('modal');
 	makeModalFrame({ 
 		el: '.resume__field, .footer__call, .footer__ask-button, .header__button, .complete__button', 
@@ -11,19 +24,18 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 		/* Коллбэк на открытие модального окошка */
 		open: function(el) {
 			const id = document.location.pathname.split('/').at(-1).split('.')[0];
+			const modal_id = [...this.closest('.modal')?.classList].at(-1);
+
 			const agree = this.querySelector('.resume__agree [type="checkbox"]');
-			const required = this.querySelectorAll('[required]');
+			const required = this.querySelectorAll('[type="tel"], [type="text"], textarea');
 			const submit = this.querySelector('[type="submit"]');
 			const alert = this.querySelector('.resume__alert');
 			const form = this.querySelector('form');
 			const rate = el.dataset.rate || '';
-			const modal_id = [...this.closest('.modal')?.classList].at(-1);
 
-			console.log(modal_id);
-	
 			const loader = document.createElement("img");
-			loader.src = 'images/loader.svg';
-	
+			loader.src = './images/loader.svg';
+			
 			/* Маска на ввод телефонного номера */
 			Inputmask({ 
 				"mask": "+7 (999) 999-99-99", 
@@ -40,7 +52,7 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 				submit.addEventListener('click', (e) => {
 					e.preventDefault();
 		
-					if ([...required].every(field => field.value)) {
+					if (fieldsValidation(required)) {
 						alert.replaceChildren();
 						alert.appendChild(loader);
 				
@@ -54,10 +66,10 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 							alert.replaceChildren();
 							
 							if(response?.success) {
-								/* Еще одна модалка с адресом из data-modal формы */
+								// Еще одна модалка с адресом из data-modal формы
 								makeModalFrame.call(form, {
 									scrollLock,
-									/* Коллбэк на открытие модального окошка */
+									// Коллбэк на открытие модального окошка
 									open: function(el) {
 										if (form.dataset.modal == 'order-complete-3') {
 											setTimeout(() => { window.location.assign('./connect.html') }, 20 * 1000);
@@ -66,7 +78,7 @@ import { addUnderlay, makeModalFrame } from "../../js/libs/modal";
 								});
 	
 							} else {
-								/* Если сервер захочет передать текстовый ответ */
+								// Если сервер захочет передать текстовый ответ
 								alert.innerText = response?.data?.text;
 							}
 	
